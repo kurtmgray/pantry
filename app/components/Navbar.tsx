5a2d2e4827a69c78d2bffdd40e3d410ea0d3efa2
scvkurt03 <kurtgray@gmail.com>
2023-06-06 20:36:49 -0700

    add .

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,21 +3,32 @@
 import { LoginButton, LogoutButton } from "./AuthButtons";
 import { useSession } from "next-auth/react";
 import { CustomSession } from "@/lib/types";
+import Links from "./Links";
 
 export default function Navbar() {
   const session = useSession();
   const sessionData = session.data as CustomSession;
   return (
     <>
-      {session?.status === "loading" ? (
-        <div>loading</div>
-      ) : (
-        <nav>
-          <p>{JSON.stringify(sessionData.user?.id, null, 4)}</p>
-          <h1>App Navbar</h1>
-          {!session ? <LoginButton /> : <LogoutButton />}
-        </nav>
-      )}
+      <nav
+        className="navbar"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <h1>App Navbar</h1>
+        {session?.status === "loading" ? (
+          <div>loading session...</div>
+        ) : (
+          <>
+            <p>User ID: {JSON.stringify(sessionData.user?.id, null, 4)}</p>
+            {session && <Links />}
+          </>
+        )}
+        {!session ? <LoginButton /> : <LogoutButton />}
+      </nav>
     </>
   );
 }
