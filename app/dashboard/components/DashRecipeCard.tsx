abbd96c4f1d78eb18afd77e1729e0bea69661308
scvkurt03 <kurtgray@gmail.com>
2023-06-26 14:12:27 -0700

    nav, dash styling

--- app/dashboard/components/DashRecipeCard.tsx
+++ app/dashboard/components/DashRecipeCard.tsx
@@ -8,8 +8,13 @@ export default function DashRecipeCard({ recipe }: Props) {
     <div className="dash__recipe_card">
       <Link href={`/recipes/${recipe.id}`} className="dash__recipe__card-link">
         <div className="dash__recipe__card-container">
-          <h2>{recipe.title}</h2>
-          <h3>{recipe.summary}</h3>
+          <img src="https://picsum.photos/150" alt={`${recipe.title} image`} />
+
+          {/* <img src={recipe.imgUrl} alt={`${recipe.title} image`}/> */}
+          <div className="dash__recipe_card-text">
+            <h2>{recipe.title}</h2>
+            <h3>{recipe.summary}</h3>
+          </div>
         </div>
       </Link>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { LoginButton, LogoutButton } from "./components/AuthButtons";
 import { CustomSession } from "@/lib/types";
+import Link from "next/link";
 
 export default async function Home() {
   const session: CustomSession | null = await getServerSession(authOptions);
-  const user = JSON.stringify(session?.user, null, 4);
+  const user = session?.user;
   return (
-    <main>
-      <LoginButton />
-      <LogoutButton />
-      {user ? <pre>{user}</pre> : <p>No user found.</p>}
+    <main className="landing-page">
+      <h1>
+        {user ? `Welcome to Pantry, ${user.name}!` : "Welcome to Pantry!"}
+      </h1>
+      <p>Discover amazing recipes and start cooking today!</p>
+      <div className="landing-buttons">{!user && <LoginButton />}</div>
+      <Link href="/register">Register</Link>
     </main>
   );
 }
